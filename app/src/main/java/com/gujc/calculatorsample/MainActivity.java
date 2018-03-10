52991be3b167f159a670cb4bff39a5c6cc561256
gujc71 <gujc71@gmail.com>
2018-03-10 22:39:55 +0900

    first commit

--- app/src/main/java/com/gujc/calculatorsample/MainActivity.java
+++ app/src/main/java/com/gujc/calculatorsample/MainActivity.java
@@ -17,21 +17,40 @@ public class MainActivity extends AppCompatActivity {
 
         editText= findViewById(R.id.editText);
 
-        button0 = findViewById(R.id.button0);
-
-        button0.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                editText.setText( button0.getText());
-            }
-        });
-
-        Button button1 = findViewById(R.id.button1);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Button a= (Button) view;
-                editText.setText(a.getText());
-            }
-        });
+        findViewById(R.id.button0).setOnClickListener(mClickListener);
+        findViewById(R.id.button1).setOnClickListener(mClickListener);
+        findViewById(R.id.button2).setOnClickListener(mClickListener);
+        findViewById(R.id.button3).setOnClickListener(mClickListener);
+        findViewById(R.id.button4).setOnClickListener(mClickListener);
+        findViewById(R.id.button5).setOnClickListener(mClickListener);
+        findViewById(R.id.button6).setOnClickListener(mClickListener);
+        findViewById(R.id.button7).setOnClickListener(mClickListener);
+        findViewById(R.id.button8).setOnClickListener(mClickListener);
+        findViewById(R.id.button9).setOnClickListener(mClickListener);
+        findViewById(R.id.button10).setOnClickListener(mClickListener);
+        findViewById(R.id.button11).setOnClickListener(mClickListener);
+        findViewById(R.id.button12).setOnClickListener(mClickListener);
+        findViewById(R.id.button13).setOnClickListener(mClickListener);
+        findViewById(R.id.button14).setOnClickListener(mClickListener);
     }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            Button a= (Button) view;
+            editText.setText(editText.getText().toString() + a.getText().toString());
+        }
+    };
+    /*Button.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.button0:
+                    editText.setText("0");
+                    break;
+                case R.id.button1:
+                    editText.setText("1");
+                    break;
+            }
+        }
+    };*/
+
 }
